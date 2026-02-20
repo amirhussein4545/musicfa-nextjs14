1368420645203c1405c074953e47ab39ce481557
Amirhussein <amirhussein0one@gmail.com>
2026-02-20 16:17:45 +0330

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = { content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'], theme: { extend: { colors: { primary: '#8B5CF6', secondary: '#EC4899', }, }, }, darkMode: 'class', plugins: [], }
